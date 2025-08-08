7174814c152e74ea1302158e5dbb16791bfaa96b
Thoeun Soklin <89516033+linsok@users.noreply.github.com>
2025-08-08 22:51:31 +0700

    Update index.js

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch(`${BASE_API_URL}/auth/login/`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username: username, password: password })
+   body: JSON.stringify({ email: username, password: password })
   })
   .then(async res => {
     if (!res.ok) {
