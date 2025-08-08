125346e5bceaf7321d229e1173d55431d7229cfa
Thoeun Soklin <89516033+linsok@users.noreply.github.com>
2025-08-08 20:55:17 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -159,3 +159,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SITE_ID = 1
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
+
